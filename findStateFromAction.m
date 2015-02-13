5204854c0c7e5692ed4ff990aff8cf9b73bb844d
alulam <alula.2@osu.edu>
2015-02-13 16:00:01 -0500

    Modified findActionsFromState

--- findStateFromAction.m
+++ findStateFromAction.m
@@ -1,30 +1,30 @@
-function[child]= actions(node,action)
-    child=node;
+function[child]= findStateFromActions(state,action)
+    child=state;
     %find blank square, remember MATLAB begins indices at 1
     index=1;
-    while node(index)~=0
+    while state(index)~=0
         index=index+1;
     end
 
     %action 1 == UP
-    if  action==1 && index>3
+    if  action==1 
         temp=child(index);
         child(index)=child(index-3);
         child(index-3)=temp; 
 
      %action 2 == DOWN
-    elseif action==2 && index<7
+    elseif action==2 
         temp= child(index);
         child(index)= child(index+3);
         child(index+3)= temp;
 
      %action 3 == LEFT
-    elseif action==3 && (mod(index,3)~=1)
+    elseif action==3 
         temp= child(index);
         child(index)= child(index-1);
         child(index-1)= temp;
      %action 4 == RIGHT
-    elseif action==4 && (mod(index,3)~=0)
+    elseif action==4 
         temp= child(index);
         child(index)= child(index+1);
         child(index+1)= temp;
